b1d68640c0786cf58a890b9d26497a8ac63c0c2b
Kaynapse <martinmcwg@gmail.com>
2025-03-09 14:33:18 +0200

    working on main menu, trying to reset score sum to 0 after unpausing the game

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ import sys
 from shot import *
 import background
 from playerscore import *
+from button import Button
+
+pygame.font.init()
+background_ = background.Background("assets/heic1304c.jpg", [0, 0])
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def get_font(size): # Returns Press-Start-2P in the desired size
+    
+    return pygame.font.Font("assets/FiraCode-Regular.ttf", size)
 
 
 def main():
@@ -32,17 +41,27 @@ def main():
 
     # create objects
     asteroid_field = AsteroidField()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    background_ = background.Background("assets/heic1304c.jpg", [0, 0])
     player_score = PlayerScore(15, 15)
     
-
+    
+    
     #game loop
-    while True:
+    game_on = True
+    while game_on:
+        
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    player_score.score_sum = 0
+                   # main_menu()
+                   # game_on = False
+                
+                
             
        
         
@@ -52,9 +71,7 @@ def main():
         for asteroid in asteroids:
             if player.check_collision(asteroid):
                 print('Game Over')
-                print(f'asteroid position: {asteroid.position.x, asteroid.position.y, asteroid.radius}')
-                print(f'player position: {player.position.x, player.position.y}')
-                pygame.time.wait(10000)
+                pygame.time.wait(1000)
                 sys.exit()
             
             for shot in shots:
@@ -80,5 +97,69 @@ def main():
         
         pygame.display.flip()
         dt = time_clock.tick(60) / 1000
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
+
+def main_menu(): # Main Menu screen
+    pygame.display.set_caption("Menu")
+
+    while True:
+        
+        screen.blit(background_.image, background_.rect)
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, (255, 255, 255))
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), 
+                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400), 
+                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550), 
+                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        
+        screen.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(screen)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    main()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pass
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+#----------------------------------- pause screen ---------------------------
+def paused():
+    pause = True
+
+    largeText = pygame.font.Font("assets/FiraCode-Regular.ttf",32)
+    TextSurf, TextRect = text_objects("Paused", largeText)
+    TextRect.center = ((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
+    screen.blit(TextSurf, TextRect)
+    
+
+    while pause:
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        #gameDisplay.fill(white)
+
+def resume():
+
+    pass
+
+main_menu()
